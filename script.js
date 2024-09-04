293f3eb8bf17941b7070e0f92170125c68c08442
riteshvish02 <rv504263@gmail.com>
2024-09-05 05:09:58 +0530

    ff

--- script.js
+++ script.js
@@ -279,3 +279,90 @@ function page12Animation(){
     })
   }
   page12Animation()
+
+  var menuToggle = document.getElementById("menuToggle");
+
+var menuBar = gsap.timeline();
+
+menuBar.to('.bar-1', 0.5,{
+	attr:{d: "M8,2 L2,8"},
+	x:1,
+	ease: Power2.easeInOut
+}, 'start')
+
+menuBar.to('.bar-2', 0.5,{
+	autoAlpha: 0
+}, 'start')
+
+menuBar.to('.bar-3', 0.5,{
+	attr:{d: "M8,8 L2,2"},
+	x:1,
+	ease: Power2.easeInOut
+}, 'start')
+
+menuBar.reverse();
+
+
+var tl1 = gsap.timeline({ paused: true});
+
+tl1.to('.fullpage-menu', {
+	duration:0,
+	display: "block",
+	ease: 'Expo.easeInOut',
+});
+
+tl1.from('.menu-bg span', {
+	duration:1,
+	x:"100%",
+	stagger: 0.1,
+	ease: 'Expo.easeInOut'
+});
+
+tl1.from('.main-menu li a', {
+	duration:1.5,
+	y:"100%",
+	stagger: 0.2,
+	ease: 'Expo.easeInOut'
+} , "-=0.5");
+
+tl1.from('.social-links li', {
+	duration:1,
+	y:"-100%",
+	opacity:0,
+	stagger: 0.1,
+	ease: 'Expo.easeInOut'
+} , "-=0.5");
+
+tl1.reverse();
+
+menuToggle.addEventListener('click', function(){
+	menuBar.reversed(!menuBar.reversed());
+	tl1.reversed(!tl1.reversed());
+});
+
+
+
+function page3SvgPathAnimation(){
+    let path = document.querySelector('.line-progress');
+let pathLength = path.getTotalLength();
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength;
+path.style.strokeDashoffset = pathLength;
+path.style.stroke = "#E5E6E8"; // Set default gray color
+
+gsap.to(path, {
+    strokeDashoffset: 0, // This will animate the strokeDashoffset to 0
+    stroke: "url(#line-progress)", // Change to blue gradient on scroll
+    duration: 15,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".one", // The container you're scrolling
+        scroller: "#main", // Locomotive Scroll scroller
+        start: "top 50%",
+        end: "top -100%",
+        scrub: true,
+        markers: true, 
+    }
+});
+}
+page3SvgPathAnimation()
