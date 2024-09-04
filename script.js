0232431ecb7ea176bc684048a0a91656095c472a
riteshvish02 <rv504263@gmail.com>
2024-09-05 04:09:32 +0530

    aaddd

--- script.js
+++ script.js
@@ -76,191 +76,50 @@ function canva2() {
 
     function files(index) {
         var data = `
-    ./garvit senior canvas/van00001.png
-    ./garvit senior canvas/van00002.png
-    ./garvit senior canvas/van00003.png
-    ./garvit senior canvas/van00004.png
-    ./garvit senior canvas/van00005.png
-    ./garvit senior canvas/van00006.png
-    ./garvit senior canvas/van00007.png
-    ./garvit senior canvas/van00008.png
-    ./garvit senior canvas/van00009.png
-    ./garvit senior canvas/van00010.png
-    ./garvit senior canvas/van00011.png
-    ./garvit senior canvas/van00012.png
-    ./garvit senior canvas/van00013.png
-    ./garvit senior canvas/van00014.png
-    ./garvit senior canvas/van00015.png
-    ./garvit senior canvas/van00016.png
-    ./garvit senior canvas/van00017.png
-    ./garvit senior canvas/van00018.png
-    ./garvit senior canvas/van00019.png
-    ./garvit senior canvas/van00020.png
-    ./garvit senior canvas/van00021.png
-    ./garvit senior canvas/van00022.png
-    ./garvit senior canvas/van00023.png
-    ./garvit senior canvas/van00024.png
-    ./garvit senior canvas/van00025.png
-    ./garvit senior canvas/van00026.png
-    ./garvit senior canvas/van00027.png
-    ./garvit senior canvas/van00028.png
-    ./garvit senior canvas/van00029.png
-    ./garvit senior canvas/van00030.png
-    ./garvit senior canvas/van00031.png
-    ./garvit senior canvas/van00032.png
-    ./garvit senior canvas/van00033.png
-    ./garvit senior canvas/van00034.png
-    ./garvit senior canvas/van00035.png
-    ./garvit senior canvas/van00036.png
-    ./garvit senior canvas/van00037.png
-    ./garvit senior canvas/van00038.png
-    ./garvit senior canvas/van00039.png
-    ./garvit senior canvas/van00040.png
-    ./garvit senior canvas/van00041.png
-    ./garvit senior canvas/van00042.png
-    ./garvit senior canvas/van00043.png
-    ./garvit senior canvas/van00044.png
-    ./garvit senior canvas/van00045.png
-    ./garvit senior canvas/van00046.png
-    ./garvit senior canvas/van00047.png
-    ./garvit senior canvas/van00048.png
-    ./garvit senior canvas/van00049.png
-    ./garvit senior canvas/van00050.png
-    ./garvit senior canvas/van00051.png
-    ./garvit senior canvas/van00052.png
-    ./garvit senior canvas/van00053.png
-    ./garvit senior canvas/van00054.png
-    ./garvit senior canvas/van00055.png
-    ./garvit senior canvas/van00056.png
-    ./garvit senior canvas/van00057.png
-    ./garvit senior canvas/van00058.png
-    ./garvit senior canvas/van00059.png
-    ./garvit senior canvas/van00060.png
-    ./garvit senior canvas/van00061.png
-    ./garvit senior canvas/van00062.png
-    ./garvit senior canvas/van00063.png
-    ./garvit senior canvas/van00064.png
-    ./garvit senior canvas/van00065.png
-    ./garvit senior canvas/van00066.png
-    ./garvit senior canvas/van00067.png
-    ./garvit senior canvas/van00068.png
-    ./garvit senior canvas/van00069.png
-    ./garvit senior canvas/van00070.png
-    ./garvit senior canvas/van00071.png
-    ./garvit senior canvas/van00072.png
-    ./garvit senior canvas/van00073.png
-    ./garvit senior canvas/van00074.png
-    ./garvit senior canvas/van00075.png
-    ./garvit senior canvas/van00076.png
-    ./garvit senior canvas/van00077.png
-    ./garvit senior canvas/van00078.png
-    ./garvit senior canvas/van00079.png
-    ./garvit senior canvas/van00080.png
-    ./garvit senior canvas/van00081.png
-    ./garvit senior canvas/van00082.png
-    ./garvit senior canvas/van00083.png
-    ./garvit senior canvas/van00084.png
-    ./garvit senior canvas/van00085.png
-    ./garvit senior canvas/van00086.png
-    ./garvit senior canvas/van00087.png
-    ./garvit senior canvas/van00088.png
-    ./garvit senior canvas/van00089.png
-    ./garvit senior canvas/van00090.png
-    ./garvit senior canvas/van00091.png
-    ./garvit senior canvas/van00092.png
-    ./garvit senior canvas/van00093.png
-    ./garvit senior canvas/van00094.png
-    ./garvit senior canvas/van00095.png
-    ./garvit senior canvas/van00096.png
-    ./garvit senior canvas/van00097.png
-    ./garvit senior canvas/van00098.png
-    ./garvit senior canvas/van00099.png
-    ./garvit senior canvas/van00100.png
-    ./garvit senior canvas/van00101.png
-    ./garvit senior canvas/van00102.png
-    ./garvit senior canvas/van00103.png
-    ./garvit senior canvas/van00104.png
-    ./garvit senior canvas/van00105.png
-    ./garvit senior canvas/van00106.png
-    ./garvit senior canvas/van00107.png
-    ./garvit senior canvas/van00108.png
-    ./garvit senior canvas/van00109.png
-    ./garvit senior canvas/van00110.png
-    ./garvit senior canvas/van00111.png
-    ./garvit senior canvas/van00112.png
-    ./garvit senior canvas/van00113.png
-    ./garvit senior canvas/van00114.png
-    ./garvit senior canvas/van00115.png
-    ./garvit senior canvas/van00116.png
-    ./garvit senior canvas/van00117.png
-    ./garvit senior canvas/van00118.png
-    ./garvit senior canvas/van00119.png
-    ./garvit senior canvas/van00120.png
-    ./garvit senior canvas/van00121.png
-    ./garvit senior canvas/van00122.png
-    ./garvit senior canvas/van00123.png
-    ./garvit senior canvas/van00124.png
-    ./garvit senior canvas/van00125.png
-    ./garvit senior canvas/van00126.png
-    ./garvit senior canvas/van00127.png
-    ./garvit senior canvas/van00128.png
-    ./garvit senior canvas/van00129.png
-    ./garvit senior canvas/van00130.png
-    ./garvit senior canvas/van00131.png
-    ./garvit senior canvas/van00132.png
-    ./garvit senior canvas/van00133.png
-    ./garvit senior canvas/van00134.png
-    ./garvit senior canvas/van00135.png
-    ./garvit senior canvas/van00136.png
-    ./garvit senior canvas/van00137.png
-    ./garvit senior canvas/van00138.png
-    ./garvit senior canvas/van00139.png
-    ./garvit senior canvas/van00140.png
-    ./garvit senior canvas/van00141.png
-    ./garvit senior canvas/van00142.png
-    ./garvit senior canvas/van00143.png
-    ./garvit senior canvas/van00144.png
-    ./garvit senior canvas/van00145.png
-    ./garvit senior canvas/van00146.png
-    ./garvit senior canvas/van00147.png
-    ./garvit senior canvas/van00148.png
-    ./garvit senior canvas/van00149.png
-    ./garvit senior canvas/van00150.png
-    ./garvit senior canvas/van00151.png
-    ./garvit senior canvas/van00152.png
-    ./garvit senior canvas/van00153.png
-    ./garvit senior canvas/van00154.png
-    ./garvit senior canvas/van00155.png
-    ./garvit senior canvas/van00156.png
-    ./garvit senior canvas/van00157.png
-    ./garvit senior canvas/van00158.png
-    ./garvit senior canvas/van00159.png
-    ./garvit senior canvas/van00160.png
-    ./garvit senior canvas/van00161.png
-    ./garvit senior canvas/van00162.png
-    ./garvit senior canvas/van00163.png
-    ./garvit senior canvas/van00164.png
-    ./garvit senior canvas/van00165.png
-    ./garvit senior canvas/van00166.png
-    ./garvit senior canvas/van00167.png
-    ./garvit senior canvas/van00168.png
-    ./garvit senior canvas/van00169.png
-    ./garvit senior canvas/van00170.png
-    ./garvit senior canvas/van00171.png
-    ./garvit senior canvas/van00172.png
-    ./garvit senior canvas/van00173.png
-    ./garvit senior canvas/van00174.png
-    ./garvit senior canvas/van00175.png
-    ./garvit senior canvas/van00176.png
-    ./garvit senior canvas/van00177.png
-    ./garvit senior canvas/van00178.png
-    ./garvit senior canvas/van00179.png
-    ./garvit senior canvas/van00180.png
-    ./garvit senior canvas/van00181.png`;
+    ./garvit senior canvas/scene00001.png
+    ./garvit senior canvas/scene00002.png
+    ./garvit senior canvas/scene00003.png
+    ./garvit senior canvas/scene00004.png
+    ./garvit senior canvas/scene00005.png
+    ./garvit senior canvas/scene00006.png
+    ./garvit senior canvas/scene00007.png
+    ./garvit senior canvas/scene00008.png
+    ./garvit senior canvas/scene00009.png
+    ./garvit senior canvas/scene00010.png
+    ./garvit senior canvas/scene00011.png
+    ./garvit senior canvas/scene00012.png
+    ./garvit senior canvas/scene00013.png
+    ./garvit senior canvas/scene00014.png
+    ./garvit senior canvas/scene00015.png
+    ./garvit senior canvas/scene00016.png
+    ./garvit senior canvas/scene00017.png
+    ./garvit senior canvas/scene00018.png
+    ./garvit senior canvas/scene00019.png
+    ./garvit senior canvas/scene00020.png
+    ./garvit senior canvas/scene00021.png
+    ./garvit senior canvas/scene00022.png
+    ./garvit senior canvas/scene00023.png
+    ./garvit senior canvas/scene00024.png
+    ./garvit senior canvas/scene00025.png
+    ./garvit senior canvas/scene00026.png
+    ./garvit senior canvas/scene00027.png
+    ./garvit senior canvas/scene00028.png
+    ./garvit senior canvas/scene00029.png
+    ./garvit senior canvas/scene00030.png
+    ./garvit senior canvas/scene00031.png
+    ./garvit senior canvas/scene00032.png
+    ./garvit senior canvas/scene00033.png
+    ./garvit senior canvas/scene00034.png
+    ./garvit senior canvas/scene00035.png
+    ./garvit senior canvas/scene00036.png
+    ./garvit senior canvas/scene00037.png
+    ./garvit senior canvas/scene00038.png
+    ./garvit senior canvas/scene00039.png
+  `;
         return data.split("\n")[index];
     }
 
-    const frameCount = 181;
+    const frameCount = 39;
 
     const images = [];
     const imageSeq = {
@@ -332,6 +191,38 @@ canva2()
 
 
 
+var tl  =  gsap.timeline()
+tl
+.to("#loader #outsideloader #prog",{
+    transform:"scaleY(1)",
+     duration:1,
+  },"c")
+  .to("#loader #outsideloader #prog",{
+    display:"none",
+     duration:1,
+    
+  },"d")
+.to("#loader #outsideloader #left",{
+x:"-100%",
+   duration:1,
+   delay:0.3,
+
+},"a")
+.to("#loader #outsideloader #right",{
+    x:"100%",
+    duration:1,
+   delay:0.3,
+
+ },"a")
+ .to("#loader #insideloader #line #progress",{
+    transform:"scaleX(1)",
+     duration:1,
+  },"e")
+  .to("#loader",{
+    display:"none",
+     duration:1,
+  },"f")
+
 
 // draggableImage.js
 
@@ -349,9 +240,42 @@ function pg5() {
     tl
         .to("#p1", { top: "0%", duration: 1.6 })
         .to("#p2", { top: "8%", duration: 1.5 })
-        .to("#p3", { top: "17%", duration: 1.5 })
-        .to("#p4", { top: "26%", duration: 1.5 })
-        .to("#p5", { top: "34%", duration: 1.5 });
+        .to("#p3", { top: "16%", duration: 1.5 })
+        .to("#p4", { top: "24%", duration: 1.5 })
+        .to("#p5", { top: "32%", duration: 1.5 });
 }
 
 pg5();
+
+function page12Animation(){
+    gsap.to(".page12-image1,.page12-image3",{
+      y:"-150",
+      duration:5,
+      scrollTrigger:{
+        scroller:"#main",
+        trigger:"#page12",
+        scrub:2,
+        // start:"top 50%",
+        // end:"top 10%",
+      }
+    })
+    gsap.to(".page12-image2,.page12-image4",{
+        y:150,
+        duration:5,
+        scrollTrigger:{
+          scroller:"#main",
+          trigger:"#page12",
+          scrub:2
+        }
+    })
+    gsap.from(".page12-line",{
+      height:"0vh",
+      duration:1,
+        scrollTrigger:{
+          scroller:"#main",
+          trigger:"#page12",
+          scrub:2
+        }
+    })
+  }
+  page12Animation()
